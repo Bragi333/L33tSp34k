0e94edd4d01dd4aa33c8a80a6028eae8dbfb4a86
Bragi333 <53411948+Bragi333@users.noreply.github.com>
2019-08-21 13:16:36 -0500

    Update L33t Sp34k.py

--- L33t Sp34k.py
+++ L33t Sp34k.py
@@ -1,27 +1,13 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# ### Cool Dank L33t Sp3aK Language BROOO
-# Changes letters to numbers to create Brian's stupid "hacker language," also known as "Leet Speak."
-
-# In[1]:
-
-
+######## Functions and Global Variables ###################1
 def hacker(text):
     for ch in ['a', 'e', 'i', 'o', 'u', 's']:
         if ch in text:
             text = text.replace('a', '4').replace('e', '3').replace('i', '1').replace('o', '0').replace('u', 'V').replace('s', '$')
     return text
 
-
-# In[2]:
-
-
 text = input("Type in a sentence and we'll l33t-i-fy it for you: ")
+###########################################################1
 
-
-# In[3]:
-
-
+############# User Commands ###############################2
 print (hacker(text))
-
+###########################################################2
